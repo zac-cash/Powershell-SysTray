b7098ff8a92fa60c770ea5be24cf058e4209fdd2
zac-cash <Zachary.cash@rate.com>
2022-08-18 11:16:52 -0500

    corrected ipv4 script with utf-8, added write-host message to copy to clipboard

--- bin/Prioritize_IPv4.ps1
+++ bin/Prioritize_IPv4.ps1
@@ -1,6 +1,4 @@
-#Check if IPv4 IP address is preferred
-ping $env:COMPUTERNAME
-#If the reply is IPv6 address, run following registry setting to just prefer ipv4 and reboot
-New-ItemProperty “HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\” -Name “DisabledComponents” -Value 0x20 -PropertyType “DWord”
-#If DisabledComponents exists, use the set cmdlet
-Set-ItemProperty “HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\” -Name “DisabledComponents” -Value 0x20
+ping $env:computername
+
+New-ItemProperty "HKLM:\system\CurrentControlSet\Services\Tcpip\Parameters\" -name "DisabledComponents" -Value 0x20 -PropertyType "Dword"
+Set-ItemProperty "HKLM:\system\CurrentControlSet\Services\Tcpip\Parameters\" -name "DisabledComponents" -Value 0x20
